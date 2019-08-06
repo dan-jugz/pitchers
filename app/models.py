703f31742a5b81b0ba39baaa74ed4d0fb63debd2
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-06 14:20:49 +0300

    setup the forms.import requirements

--- app/models.py
+++ app/models.py
@@ -7,8 +7,9 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
+    #schema
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), index=True)
     password_hash = db.Column(db.String(255), unique=True, index=True)
