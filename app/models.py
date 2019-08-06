2913677c00c743ea5c66328012001586c29fd127
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-06 16:36:45 +0300

    import datetime.create pitch class with id from users id foreign key

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db,login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -31,3 +32,11 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Pitch(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_ky=True)
+    pitch_title = db.Column(db.String)
+    pitch_body = db.Column(db.String)
+    postedAt = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id")
