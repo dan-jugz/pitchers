7d7d8d93d2c7e3616bf3b776362a6ab2012e7893
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-06 16:21:00 +0300

    set a destination for uploaded photos

--- app/__init__.py
+++ app/__init__.py
@@ -33,4 +33,8 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
+    # Configure UploadSet
+    configure_uploads(app,photos)
+
+
     return app
