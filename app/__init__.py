fa1952415abb53a9b4e604fe350160511421b610
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-06 13:40:55 +0300

    add new pitch route

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 bootstrap = Bootstrap()
 db = SQLAlchemy
 login_manager = LoginManager()
-login_manager.session.protection = 'strong'
+#login_manager.session.protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 
