b68bd17fceb90ab60f5bc1e74fa3437d30ba911e
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-06 16:30:09 +0300

    remove pass secure column

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
+   
 
     def __repr__(self):
         return 'User {}'.format(self.username)
