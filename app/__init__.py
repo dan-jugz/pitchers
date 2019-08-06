77c04eda7813b42e9ea0a5a26f7fd1258929ace5
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-06 18:56:57 +0300

    work out some typos

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_simplemde import SimpleMDE
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos', IMAGES)
-
+simple = SimpleMDE()
+mail = Mail()
 login_manager = LoginManager()
-login_manager.session.protection = 'strong'
+login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 
@@ -27,6 +29,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     simple.init_app(app)
+    mail.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
-from . import db,login_manager
+from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from . import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -13,16 +14,16 @@ class User(UserMixin, db.Model):
     #schema
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), index=True)
-    password_hash = db.Column(db.String(255), unique=True, index=True)
     email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255), unique=True, index=True)
+
+    #relationship betweeen user and pitch class
     pitch = db.relationship('Pitch', backref="user", lazy="dynamic")
-
-   
-
-    def __repr__(self):
-        return 'User {}'.format(self.username)
+    #creating a relationship between user and comments
+    comments_user=db.relationship('Comment',backref='user',lazy="dynamic")
+    
 
     @property
     def password(self):
@@ -40,5 +41,41 @@ class Pitch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pitch_title = db.Column(db.String)
     pitch_body = db.Column(db.String)
-    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
+    posted_at = db.Column(db.DateTime, default=datetime.utcnow())
     user_id = db.Column(db.Integer, db.ForeignKey("users.id")
+    category = db.Column(db.String(255))
+    posted_by = db.Column(db.String)
+    #creating a relationship between post and comment
+    pitch = db.relationship("Comment", backref="user_pitch", lazy="dynamic")
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    # @classmethod
+    # def get_pitch(cls):   
+    
+    #     #retrieve posts in descending order
+    #     posts=Posts.query.order_by(Posts.id.desc()).all()
+    #     return posts  
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    comment = db.Column(db.String)
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitch_table.id"))
+    postedAt = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    posted_by = db.Column(db.String)
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, id):
+        comments = Comment.query.filter_by(pitch_id=id)
+        return comments
+        
+
