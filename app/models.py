415877089c3002e8d7cb5ace1168905f83c74c84
daniel njuguna <njugunadaniel364@gmail.com>
2019-08-07 04:48:31 +0300

    add docstrings to model for logic flow

--- app/models.py
+++ app/models.py
@@ -16,7 +16,9 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
+    #creating a relationship between user and posts class
     pitch = db.relationship('Pitch', backref="user", lazy="dynamic")
+     #creating a relationship between user and comments
     user_comment = db.relationship('Comment', backref="user", lazy="dynamic")
 
     @property
@@ -35,12 +37,15 @@ class Pitch(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pitch_title = db.Column(db.String)
     pitch_body = db.Column(db.String)
-    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
+    posted_at = db.Column(db.DateTime, default=datetime.utcnow())
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     category = db.Column(db.String(255))
+    #creating a relationship between post and comment
     pitch = db.relationship("Comment", backref="user_pitch", lazy="dynamic")
     posted_by = db.Column(db.String)
-    
+    #creating a relationship to link a post to a user img
+    # profile_pic_path=db.relationship('User',backref='user', lazy='dynamic')
+
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
@@ -49,10 +54,10 @@ class Comment(db.Model):
     __tablename__ = "comments"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    comment = db.Column(db.String)
-    pitch_id = db.Column(db.Integer, db.ForeignKey("pitch_table.id"))
+    comment = db.Column(db.String)  #comment a user gives to a post
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitch_table.id"))  #post id for linking a comment to a specific post
     postedAt = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id")) #user id for linking a comment to a specific user
     posted_by = db.Column(db.String)
 
     def save_comment(self):
